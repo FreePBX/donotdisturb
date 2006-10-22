da4fabbda17eb120ac86b603372d07abb7041119
Diego Iastrubni <diego.iastrubni@xorcom.com>
2006-10-22 15:13:09 +0000

    updated modules to reflect the new change in policy. i did not bump up the versions, since the modules should still work, i will let the developers of each module nump up versions on their free time, in order to reduce the updates sent to the users.

--- uninstall.php
+++ uninstall.php
@@ -1,18 +1,15 @@
 <?php
 
-require_once('common/php-asmanager.php');
+// TODO, is this needed...?
+// is this global...? what if we include this files
+// from a function...?
+global $astman;
 
 // remove all D-N-D options in effect on extensions
-$astman = new AGI_AsteriskManager();
-if ($res = $astman->connect("127.0.0.1", $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"])) {
-
+if ($astman) {
 	$astman->database_deltree('DND');
-
 } else {
-
 	fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
-
 }
-$astman->disconnect();
 
 ?>
