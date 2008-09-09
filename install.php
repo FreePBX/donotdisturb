513dd55969cd1a3044b0ab2d01e178e0019236e8
Micke Carlsson <mickecamino@gmail.com>
2008-09-09 11:30:28 +0000

    Added swedish translation and enclosed some text strings for donotdisturb module

--- install.php
+++ install.php
@@ -1,22 +1,25 @@
 <?php
 
 // Register FeatureCode - Activate
+$dndactivate = _("DND Activate");
 $fcc = new featurecode('donotdisturb', 'dnd_on');
-$fcc->setDescription('DND Activate');
+$fcc->setDescription($dndactivate);
 $fcc->setDefault('*78');
 $fcc->update();
 unset($fcc);
 
 // Register FeatureCode - Deactivate
+$dnddeactivate = _("DND Deactivate");
 $fcc = new featurecode('donotdisturb', 'dnd_off');
-$fcc->setDescription('DND Deactivate');
+$fcc->setDescription($dnddeactivate);
 $fcc->setDefault('*79');
 $fcc->update();
 unset($fcc);	
 
 // Register FeatureCode - Activate
+$dndtoggle = _("DND Toggle");
 $fcc = new featurecode('donotdisturb', 'dnd_toggle');
-$fcc->setDescription('DND Toggle');
+$fcc->setDescription($dndtoggle);
 $fcc->setDefault('*76');
 $fcc->update();
 unset($fcc);
