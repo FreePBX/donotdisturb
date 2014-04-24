64d1fb4541af0c6153f8973e277a38993f041cc6
Andrew Nagy <andrew.nagy@the159.com>
2014-04-23 22:10:37 -0400

    Help text in UCP

--- ucp/views/settings.php
+++ ucp/views/settings.php
@@ -1,7 +1,7 @@
 <div class="message alert" style="display:none;"></div>
 <form role="form">
-	<label>
-		<?php echo _('Enable')?>
+	<div class="form-group">
+		<label for="dndenable-h" class="help"><?php echo _('Enable')?> <i class="fa fa-question-circle"></i></label>
 		<div class="onoffswitch">
 			<input type="checkbox" name="dndenable" class="onoffswitch-checkbox" id="dndenable" <?php echo ($enabled) ? 'checked' : ''?>>
 			<label class="onoffswitch-label" for="dndenable">
@@ -9,5 +9,6 @@
 				<div class="onoffswitch-switch"></div>
 			</label>
 		</div>
-	</label>
+		<span class="help-block help-hidden" data-for="dndenable-h"><?php echo _('Used to indicate that somebody does not wish to be disturbed.')?></span>
+	</div>
 </form>
