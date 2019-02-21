6113fb789e031e79e713ef6d84fc6f652c202100
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 15:16:58 -0500

    [Module Tag script: donotdisturb 15.0.4] fixing legacy restore

--- Backup.php
+++ Backup.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Donotdisturb;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
- 
+
     $configs = $this->FreePBX->Donotdisturb->getAllStatuses();
     $this->addConfigs($configs);
   }
--- Restore.php
+++ Restore.php
@@ -2,27 +2,27 @@
 namespace FreePBX\modules\Donotdisturb;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    $dnd = $this->FreePBX->Donotdisturb;
-    /**
-     * Clear existing
-     */
-    foreach($dnd->getAllStatuses() as $key => $value){
-        $dnd->setStatusByExtension(str_replace('/DND/', '', $key));
-    }
-    foreach($configs as $key => $value){
-        $dnd->setStatusByExtension(str_replace('/DND/', '', $key), $value);
-    }
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $dnd = $this->FreePBX->Donotdisturb;
-    $astdb = $this->getAstDb($tmpfiledir . '/astdb');
-    if (!isset($astdb['DND'])) {
-      return $this;
-    }
-    foreach ($astdb['DND'] as $key => $value) {
-      $dnd->setStatusByExtension(str_replace('/DND/', '', $key), $value);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$dnd = $this->FreePBX->Donotdisturb;
+		/**
+		 * Clear existing
+		 */
+		foreach($dnd->getAllStatuses() as $key => $value){
+				$dnd->setStatusByExtension(str_replace('/DND/', '', $key));
+		}
+		foreach($configs as $key => $value){
+				$dnd->setStatusByExtension(str_replace('/DND/', '', $key), $value);
+		}
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$dnd = $this->FreePBX->Donotdisturb;
+		$astdb = $data['astdb'];
+		if (!isset($astdb['DND'])) {
+			return;
+		}
+		foreach ($astdb['DND'] as $key => $value) {
+			$dnd->setStatusByExtension(str_replace('/DND/', '', $key), $value);
+		}
+	}
 }
