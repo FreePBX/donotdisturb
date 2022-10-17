bfc27fb8da8ff756ec7c9e059329710289897f5b
S Tejaswini <stejaswini@sangoma.com>
2022-10-17 09:55:19 +0000

    improvement/bring-17-to-same-as-16 --> CSS changes based on Freepbx 16 dev system

--- ucp/views/widget.php
+++ ucp/views/widget.php
@@ -1,3 +1,3 @@
-<div class="text-center">
+<div class="text-center dndenable-padding">
 	<input type="checkbox" name="dndenable" data-toggle="toggle" <?php echo ($enabled) ? 'checked' : ''?> data-on="<?php echo _("Enabled")?>" data-off="<?php echo _("Disabled")?>">
 </div>
